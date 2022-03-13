3c440200ce6813e6f209a7b71d20325c316db4db
André <andre.sanches.1.12@gmail.com>
2022-03-12 22:42:20 -0400

    add src files

---
+++ constantes.hpp
@@ -0,0 +1,23 @@
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#define APP_WIDTH     540
+#define APP_HEIGHT    320
+#define APP_NAME      "PONG"
+#define FPS           60
+#define OESTE         0
+#define NOROESTE      1
+#define SUDOESTE      2
+#define LESTE         3
+#define NORDESTE      4
+#define SUDESTE       5
+#define SINGLEPLAYER  1
+#define MULTIPLAYER   2
+#define MENU_SCREEN   1
+#define GAME_SCREEN   2
+#define DIFICULDADE   90
+#define WHITE         makecol(255, 255, 255)
+#define RED           makecol(255, 0, 0)
+#define SHADOW	      makecol(25, 25, 25)
+
+#endif
